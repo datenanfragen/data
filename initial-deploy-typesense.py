c3c569da0426b57970500a2bcc4a6da9204601f1
Berengar W. Lehr <Berengar.Lehr@kompetenztest.de>
2019-10-21 00:50:29 +0200

    Create initial deploy script

---
+++ initial-deploy-typesense.py
@@ -0,0 +1,28 @@
+import typesense
+import os
+import json
+
+client = typesense.Client({
+    'master_node': {
+        'host': '127.0.0.1',
+        'port': '8108',
+        'protocol': 'http',
+        'api_key': os.environ['TYPESENSE_API_KEY']
+    },
+    'timeout_seconds': 2
+})
+
+def check_or_create_collection(index, schema_file):
+    fields = [{'name': prop, 'type': details['type'] if details['type'] != 'array' else details['items']['type'] + '[]'} for prop, details in json.load(open(schema_file, encoding = 'utf-8'))['properties'].items()]
+    schema =  {
+        'name': index,
+        'fields': fields,
+        'default_sorting_field': 'sort-index'
+    }
+    print(schema)
+    if (index in [entry['name'] for entry in client.collections.retrieve()]):
+        client.collections[index].delete()
+    client.collections.create(schema)
+
+check_or_create_collection('companies', 'schema.json')
+check_or_create_collection('supervisory-authorities', 'schema-supervisory-authorities.json')
