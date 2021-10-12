142e215a6ffdd51417fbc1dee86e50c8979e52b9
mal-tee <45886910+mal-tee@users.noreply.github.com>
2021-10-12 12:46:42 +0200

    ich kran nicht mehr

--- dog.sh
+++ dog.sh
@@ -2,9 +2,10 @@
 
 yarn ts-node src/test-records.ts --autofix
 # https://github.com/reviewdog/reviewdog#diff
+ls
 F=${mktemp}
-echo "${F}"
+echo "F: -${F}-"
 git diff >"${F}"
-git stash -u
-git stash drop
+git clean -fd
+git clean -f
 ./bin/reviewdog -f=diff -f.diff.strip=1 -reporter=github-pr-review <"${F}"
