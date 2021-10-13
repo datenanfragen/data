9add3a53d9ed66678840197aa225a5a202cf5b29
mal-tee <45886910+mal-tee@users.noreply.github.com>
2021-10-13 16:09:35 +0200

    cleanup

--- dog.sh
+++ dog.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
 yarn ts-node src/test-records.ts --auto-fix || true
 # https://github.com/reviewdog/reviewdog#diff
-ls
 F=$(mktemp)
-echo "F: -${F}-"
 git diff >"${F}"
 git clean -fd
 git clean -f
-echo "---"
-cat "${F}"
-echo "---"
 ~/bin/reviewdog -f=diff -f.diff.strip=1 -reporter=github-pr-review -tee <"${F}"
