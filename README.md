deb4a020d941c0b97263cd4941cf292b97565972
Malte <mal-tee@mailbox.org>
2021-12-01 12:46:20 +0100

    squash

--- README.md
+++ README.md
@@ -84,3 +84,9 @@ Usually, the `request-language` should not be set, except for the following case
 * If both of the following conditions are met, the `request-language` should be set to the language from the first condition:
     1. The company in question only addresses consumers in one language.
     2. The `relevant-countries` for the company include ones where this language is not spoken.
+
+## Relevant Countries
+
+`relevant-countries` is a list of countries the record is relevant for. The countries can either be an ISO 3166-1 alpha-2 country code or 'all'. If a company has branches in different countries with unique contact data, separate records should be created for those. If a company branch offers its services in English and isn't explicitly restricted to certain countries, 'all' should be specified.
+
+If 'all' is specified, no additional country should be specified, as 'all' covers all countries.
--- src/checks/company/relevant-countries-all.ts
+++ src/checks/company/relevant-countries-all.ts
@@ -3,16 +3,16 @@ import { CompanyCheck } from '../../types/checks';
 const check: CompanyCheck = {
     id: 'relevant-countries-all',
     desc: 'If `relevant-countries` contains `all`, it should only contain `all`.',
+    url: 'https://github.com/datenanfragen/data#relevant-countries',
     severity: 'ERROR',
     run: (json) => {
         if (json['relevant-countries']) {
-            const all_and = json['relevant-countries'].includes("all") && json["relevant-countries"].length > 1
-            if (all_and)
+            if (json['relevant-countries'].includes("all") && json["relevant-countries"].length > 1)
                 return {
                     message: `Record has redundant \`relevant-countries\`: \`all\` already covers: ${json["relevant-countries"].filter(x => x !== "all").join(",")}.`,
 
                     json_pointer: '/relevant-countries',
-                    suggestions: ["all"] // should this be an array of arrays?
+                    suggestions: [["all"]]
                 };
         }
     },
