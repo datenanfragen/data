2bbceb3fe3bc5ee51302c4c3e8748b6da51b241b
Malte Wessels <malte.wessels@tu-braunschweig.de>
2024-11-06 21:03:36 +0100

    run unusual whitespace check on arrays as well

--- src/checks/record/unusual-whitespace.ts
+++ src/checks/record/unusual-whitespace.ts
@@ -1,4 +1,17 @@
-import { Check } from '../../types/checks';
+import { Check, CheckInstance } from '../../types/checks';
+
+const reg = /[\u00A0\u1680\u180E\u2000-\u200B\u202F\u205F\u3000\uFEFF]/;
+
+const sub_check = (key: string, value: string | (string | undefined)[] | undefined, nested = false) => {
+    if (typeof value === 'string' && reg.test(value))
+        return {
+            message: `Value of string field \`${key}\` contains non-ASCII spaces.`,
+            json_pointer: `/${key}`,
+            suggestions: [value.replace(reg, ' ')],
+        };
+    return undefined;
+}
+
 
 const check: Check = {
     id: 'unusual-whitespace',
@@ -7,18 +20,21 @@ const check: Check = {
     severity: 'ERROR',
     run: (json) => {
         // https://stackoverflow.com/a/51399843/
-        const reg = /[\u00A0\u1680\u180E\u2000-\u200B\u202F\u205F\u3000\uFEFF]/;
-        return (Object.keys(json) as (keyof typeof json)[]).map((key) => {
-            const value = json[key];
-            if (typeof value === 'string' && reg.test(value))
-                return {
-                    message: `Value of string field \`${key}\` contains non-ASCII spaces.`,
-                    json_pointer: `/${key}`,
-                    suggestions: [value.replace(reg, ' ')],
-                };
 
-            return;
-            // TODO: Also deal with arrays and nested string fields.
+        return (Object.keys(json) as (keyof typeof json)[]).flatMap((key) => {
+            const value = json[key];
+
+            const results: (CheckInstance | undefined)[] = [];
+            results.push(sub_check(key, value));
+
+            if (Array.isArray(value)) {
+                for (const element of value) {
+                    results.push(sub_check(key, element, true));
+                }
+            }
+
+            return results;
+            // TODO: Also deal with nested string fields.
         });
     },
 };
